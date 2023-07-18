60dbd33ea429c6ccfaa3775dd20138449af1e2d4
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2023-07-18 10:07:22 -0300

    inclusao footer

--- all.js
+++ all.js
@@ -11,3 +11,20 @@ window.addEventListener('scroll', function() {
     section.classList.toggle('fade-in-scroll', shouldAnimate);
   });
 });
+
+
+const accordionButtons = document.querySelectorAll('.accordion__button');
+
+accordionButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const accordionContent = button.nextElementSibling;
+    button.classList.toggle('accordion__button--active');
+
+    if (button.classList.contains('accordion__button--active')) {
+      accordionContent.style.maxHeight = `${accordionContent.scrollHeight}px`;
+    } else {
+      accordionContent.style.maxHeight = '0';
+    }
+  });
+});
+
