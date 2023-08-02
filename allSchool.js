e8d5dcc5de6983be9317c8738c5f3dc09c5b9666
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2023-08-02 12:30:31 -0300

    ajuste rotas

--- allSchool.js
+++ allSchool.js
@@ -25,7 +25,8 @@ accordionButtons.forEach(button => {
   });
 });
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
-//   modal.show();
-// });
+//funcao para carregar o modal assim que abrir a pagina
+document.addEventListener("DOMContentLoaded", function() {
+  const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+  modal.show();
+});
